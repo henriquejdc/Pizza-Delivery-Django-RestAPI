6f34696b89b021fc645b2ef809b78ca6762923e4
Henrique Dalla Corte <riquejdc@gmail.com>
2022-03-17 22:00:33 -0300

    Correções para o deploy

--- pizza/settings.py
+++ pizza/settings.py
@@ -150,8 +150,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT=BASE_DIR / 'staticfiles'
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
